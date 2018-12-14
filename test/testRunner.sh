dd0a2f72cb536d4eb0ac1355e2dd5a33a770b63b
yaskier <r.ruiz.giner@gmail.com>
2018-12-14 12:28:57 +0100

    several checks passed, ready to go

--- test/testRunner.sh
+++ test/testRunner.sh
@@ -3,10 +3,11 @@ set -e
 
 echo "STARTING DOCKER SERVICE..."
 service docker start &>/dev/null
-
-echo "DOCKER SERVICE STARTED"
+echo "DOCKER SERVICE STARTED, PROCEEDING TO RUN TESTS"
 
 for test in *.bats; do
 	echo "RUNNING TEST $test"
 	bats --tap $test
 done
+
+echo "TESTS IN GREEN, EVERYTHING IS FINE"
